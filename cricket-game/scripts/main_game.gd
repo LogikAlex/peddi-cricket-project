2afe85847ad874fdb3877ac1f2550e89db439ed7
LogikAlex <136632902+LogikAlex@users.noreply.github.com>
2026-01-26 23:53:06 +0100

    started prototyping

--- cricket-game/scripts/main_game.gd
+++ cricket-game/scripts/main_game.gd
@@ -1,11 +1,38 @@
 extends Node2D
 
+@onready var ball: RigidBody2D = $ball
+@onready var start_timer: Timer = $start_timer
+@onready var countdown_label: Label = $countdown
 
-# Called when the node enters the scene tree for the first time.
+var current_state = null
+
+enum states {IDLE, SWINGING, HIT, GAME_OVER}
+
 func _ready() -> void:
-	pass # Replace with function body.
+	countdown()
+	
+	ball.freeze = true
+	current_state = states.IDLE
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("swing"):
+		current_state = states.SWINGING
+		hit_ball()
+
+func countdown():
+	start_timer.start()
+	var countdown_tween = create_tween()
+	countdown_tween.tween_property(countdown_label, "text", "3", 0)
+	countdown_tween.tween_property(countdown_label, "text", "2", 0).set_delay(1)
+	countdown_tween.tween_property(countdown_label, "text", "1", 0).set_delay(1)
+	countdown_tween.tween_property(countdown_label, "visible", false, 0).set_delay(1)
+
+func launch_ball():
+	ball.freeze = false
+	ball.apply_force(Vector2(-11000.0, -4500.0))
+
+func hit_ball():
+	ball.apply_force(Vector2(16000.0, -9300.0))
+
+func _throw_ball() -> void:
+	launch_ball()
